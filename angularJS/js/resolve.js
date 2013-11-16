640c0d382575af2b781dd888ddbceb476af8781d
villain2 <djvillain@hotmail.com>
2013-11-16 11:16:44 -0500

    updated with error handling

--- angularJS/js/resolve.js
+++ angularJS/js/resolve.js
@@ -11,6 +11,19 @@ app.config(function ($routeProvider) {
         }) 
 });
 
+app.directive("error", function ($rootScope) {
+    //check to see if we can pull this out to somewhere else
+    return {
+        restrict: "E",
+        template: '<div class="alert-box alert" ng-show="isError">Error!</div>',
+        link: function (scope) {
+             $rootScope.$on("$routeChangeError", function (event, current, previous, rejection) {
+                 scope.isError = true;
+             })
+        }
+    }
+});
+
 app.controller("AppCtrl", function ($rootScope) {
     $rootScope.$on("$routeChangeError", function (event, current, previous, rejection) {
         console.log(event);
@@ -28,7 +41,7 @@ viewCtrl.loadData = function ($q, $timeout) {
     //q library is the promise library, timeout sets a time below after function
     var defer       = $q.defer();
     $timeout(function () {
-        defer.reject('Your network is down');
+        defer.reject('Your network is down'); //change to resolve so it works
     }, 500); // will fire after 2 seconds
     return defer.promise;
 };
